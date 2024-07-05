76bf9b6b7c888cd0a3082ae4f6f8fe84a0033082
Şükrücan TIĞLIOĞLU <54224494+stiglioglu@users.noreply.github.com>
2024-07-06 02:24:56 +0300

    Created entities

--- Presentation/WebApi.Api/Program.cs
+++ Presentation/WebApi.Api/Program.cs
@@ -14,6 +14,12 @@ namespace WebApi.Api
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            var env = builder.Environment;
+            builder.Configuration
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
